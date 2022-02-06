9227f1fe4e67f6abfd0cc353d9d894178b6e2864
HP <aryamanDchobey@gmail.com>
2022-02-06 16:06:58 -0500

    questions 1 and 2

--- Activity2 Intro to Statistics and Weather/Activity2.r
+++ Activity2 Intro to Statistics and Weather/Activity2.r
@@ -1,1 +1,15 @@
+#question 1
+datW <- read.csv("C:\\Users\\HP\\Desktop\\comp sci\\GEOG331\\GEOG331\\Activity2 Intro to Statistics and Weather\\Noaa weather\\2011124.csv",
+                 stringsAsFactors = T)
+str(datW)
+#checking number of rows and columns
+nrow(datW)
+ncol(datW)
+datW$dateF <- as.Date(datW$DATE, "%Y-%m-%d")
+datW$year <- as.numeric(format(datW$dateF,"%Y"))
+# Question 2
+char_vector <-c("a","bc","def","hijk","lmnop")
+num_vector<-c(1,-2,44,3.3,0)
+int_vector<- c(1,-2,3,4,5)
+factor_vector<- factor(char_vector)
 
