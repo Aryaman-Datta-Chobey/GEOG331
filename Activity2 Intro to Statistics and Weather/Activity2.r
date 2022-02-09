d3dd3e25c01af656be5092e573949d43cb82b590
HP <aryamanDchobey@gmail.com>
2022-02-09 12:13:04 -0500

    site summary function

--- Activity2 Intro to Statistics and Weather/Activity2.r
+++ Activity2 Intro to Statistics and Weather/Activity2.r
@@ -11,22 +11,29 @@ datW$year <- as.numeric(format(datW$dateF,"%Y"))
 char_vector <-c("a","bc","def","hijk","lmnop")
 num_vector<-c(1,-2,44,3.3,0)
 int_vector<- c(1,-2,3,4,5)
-factor_vector<- factor(char_vector)
+factor_vector<- factor(char_vector) bruh
 #Question 3
 datW$siteN <- as.numeric(datW$NAME)
 datW$TAVE <- datW$TMIN + ((datW$TMAX-datW$TMIN)/2)
 #Histogram for Aberdeen
 hist(datW$TAVE[datW$siteN==1],
-     freq=FALSE, 
+     freq=FALSE,  
      main = paste(levels(datW$NAME)[1]),
      xlab = "Average daily temperature (degrees C)", 
      ylab="Relative frequency",
-     col="grey50",
+     col="grey50", 
      border="white")
+# mean line
+abline(v = mean(datW$TAVE[datW$siteN == 1],na.rm=TRUE), 
+       col = "tomato3", 
+       lty = 1,
+       lwd = 3)
+#1 standard deviation above
 abline(v = mean(datW$TAVE[datW$siteN == 1],na.rm=TRUE) + sd(datW$TAVE[datW$siteN == 1],na.rm=TRUE), 
        col = "tomato3", 
        lty = 3,
        lwd = 3)
+#1 standard deviation below
 abline(v = mean(datW$TAVE[datW$siteN == 1],na.rm=TRUE) - sd(datW$TAVE[datW$siteN == 1],na.rm=TRUE), 
        col = "tomato3", 
        lty = 3,
@@ -34,6 +41,34 @@ abline(v = mean(datW$TAVE[datW$siteN == 1],na.rm=TRUE) - sd(datW$TAVE[datW$siteN
 help(hist)
 help(paste)
 #Question 4
+#function for getting histograms with mean and standard deviation lines for different stations 
+SiteSummary<-function(SiteNumber, graphcol,linecol){
+  #intialize variables
+  datW$siteN <- as.numeric(datW$NAME)
+  datW$TAVE <- datW$TMIN + ((datW$TMAX-datW$TMIN)/2)
+  hist(datW$TAVE[datW$siteN==SiteNumber],
+       freq=FALSE,  
+       main = paste(levels(datW$NAME)[SiteNumber]),
+       xlab = "Average daily temperature (degrees C)", 
+       ylab="Relative frequency",
+       col=graphcol, 
+       border="white")
+  # mean line
+  abline(v = mean(datW$TAVE[datW$siteN == SiteNumber],na.rm=TRUE), 
+         col = linecol, 
+         lty = 1,
+         lwd = 3)
+  #1 standard deviation above
+  abline(v = mean(datW$TAVE[datW$siteN == SiteNumber],na.rm=TRUE) + sd(datW$TAVE[datW$siteN == SiteNumber],na.rm=TRUE), 
+         col = linecol, 
+         lty = 3,
+         lwd = 3)
+  #1 standard deviation below
+  abline(v = mean(datW$TAVE[datW$siteN == SiteNumber],na.rm=TRUE) - sd(datW$TAVE[datW$siteN == SiteNumber],na.rm=TRUE), 
+         col = linecol, 
+         lty = 3,
+         lwd = 3)
+}
 #Histogram for Livemore
 hist(datW$TAVE[datW$siteN==2],
      freq=FALSE, 
