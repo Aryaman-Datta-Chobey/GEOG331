b163af4cb0f4ece9c7ea6c6472a60c0a7a79e8bf
HP <aryamanDchobey@gmail.com>
2022-02-14 11:34:10 -0500

    comments

--- Activity2 Intro to Statistics and Weather/Activity2.r
+++ Activity2 Intro to Statistics and Weather/Activity2.r
@@ -1,7 +1,6 @@
 #question 1
 datW <- read.csv("C:\\Users\\HP\\Desktop\\comp sci\\GEOG331\\GEOG331\\Activity2 Intro to Statistics and Weather\\Noaa weather\\2011124.csv",
                  stringsAsFactors = T)
-str(datW)
 #checking number of rows and columns
 nrow(datW)
 ncol(datW)
@@ -11,7 +10,7 @@ datW$year <- as.numeric(format(datW$dateF,"%Y"))
 char_vector <-c("a","bc","def","hijk","lmnop")
 num_vector<-c(1,-2,44,3.3,0)
 int_vector<- c(1,-2,3,4,5)
-factor_vector<- factor(char_vector) bruh
+factor_vector<- factor(char_vector) 
 #Question 3
 datW$siteN <- as.numeric(datW$NAME)
 datW$TAVE <- datW$TMIN + ((datW$TMAX-datW$TMIN)/2)
@@ -38,11 +37,10 @@ abline(v = mean(datW$TAVE[datW$siteN == 1],na.rm=TRUE) - sd(datW$TAVE[datW$siteN
        col = "tomato3", 
        lty = 3,
        lwd = 3)
-help(hist)
-help(paste)
+
 #Question 4
 #function for getting histograms with mean and standard deviation lines for different stations 
-SiteSummary<-function(SiteNumber, graphcol,linecol){
+SiteSummary_AvgTemp<-function(SiteNumber, graphcol,linecol){
   #intialize variables
   datW$siteN <- as.numeric(datW$NAME)
   datW$TAVE <- datW$TMIN + ((datW$TMAX-datW$TMIN)/2)
@@ -69,6 +67,7 @@ SiteSummary<-function(SiteNumber, graphcol,linecol){
          lty = 3,
          lwd = 3)
   #Normal distrubtion dotted graph for q5
+  #Sequences have been set between mean-3standard deviations and mean+3 stard deviations for the X ab=nd Y axis to create a more universersal probability density function plot than -10,30 which is mainly apropriate for aberdeen
   x.plot <- seq(mean(datW$TAVE[datW$siteN == SiteNumber],na.rm=TRUE) - 3*sd(datW$TAVE[datW$siteN == SiteNumber],na.rm=TRUE),mean(datW$TAVE[datW$siteN == SiteNumber],na.rm=TRUE) + 3*sd(datW$TAVE[datW$siteN == SiteNumber],na.rm=TRUE), length.out = 100)
   y.plot <-  dnorm(seq(mean(datW$TAVE[datW$siteN == SiteNumber],na.rm=TRUE) - 3*sd(datW$TAVE[datW$siteN == SiteNumber],na.rm=TRUE),mean(datW$TAVE[datW$siteN == SiteNumber],na.rm=TRUE) + 3*sd(datW$TAVE[datW$siteN == SiteNumber],na.rm=TRUE), length.out = 100),
                    mean(datW$TAVE[datW$siteN == SiteNumber],na.rm=TRUE),
@@ -82,14 +81,15 @@ SiteSummary<-function(SiteNumber, graphcol,linecol){
          lty = 2)
 }
 par(mfrow=c(2,2))
-SiteSummary(1,"grey50","tomato3") #Histogram for Aberdeen
-SiteSummary(2,"purple","tomato3") #Histogram for Livemore
-SiteSummary(3,"green","tomato3") #Histogram for Mandan Experiment Station
-SiteSummary(4,"yellow","tomato3") #Histogram for Mormon flat
+SiteSummary_AvgTemp(1,"grey50","tomato3") #Histogram for Aberdeen
+SiteSummary_AvgTemp(2,"purple","tomato3") #Histogram for Livemore
+SiteSummary_AvgTemp(3,"green","tomato3") #Histogram for Mandan Experiment Station
+SiteSummary_AvgTemp(4,"yellow","tomato3") #Histogram for Mormon flat
 
-
+#Question 6
 qnorm(0.95,
       mean(4+datW$TAVE[datW$siteN == 1],na.rm=TRUE),
       sd(datW$TAVE[datW$siteN == 1],na.rm=TRUE))
 help(qnorm)
+#Question 7
 
