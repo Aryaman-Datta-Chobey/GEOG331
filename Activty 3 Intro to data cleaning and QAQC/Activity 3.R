2fb3d3bd50e0b12ee97b8d6fee6957a797e7f85d
HP <aryamanDchobey@gmail.com>
2022-03-07 09:49:02 -0500

    finished actvity 3

--- Activty 3 Intro to data cleaning and QAQC/Activity 3.R
+++ Activty 3 Intro to data cleaning and QAQC/Activity 3.R
@@ -53,7 +53,7 @@ points(datW$DD[datW$precipitation > 0], datW$precipitation[datW$precipitation > 
 #plot lightning points only when there is lightning     
 points(datW$DD[lightscale > 0], lightscale[lightscale > 0],
        col= "tomato3", pch=19)
-# QUESTION 5
+# Question 5
 assert(length(datW$precipitation) == length(datW$lightning.acvitivy),
        "error: unequal length")
 
@@ -65,16 +65,10 @@ datW$air.tempQ2 <- ifelse(datW$precipitation  >= 2 & datW$lightning.acvitivy >0,
 #QUESTION 6- QA/QC for windspeed, filtering out data collected during storms , defined by lightning that coincides with rainfall greater than 2mm or only rainfall over 5 mm.
 datW$wind.speedQ1 <- ifelse(datW$precipitation  >= 2 & datW$lightning.acvitivy >0, NA,
                           ifelse(datW$precipitation > 5, NA, datW$wind.speed))
-print(length(is.na(datW$wind.speedQ1)==FALSE))
-assert(length(is.na(datW$wind.speedQ1)==FALSE) == length(datW$wind.speedQ1),
+#Assert function comparing the number of NA values in the quality controled vector for windspeed with number of suspect values in the oringal windspeed vector
+assert(length((is.na(datW$wind.speedQ1)[which(is.na(datW$wind.speedQ1==TRUE))])) == length(datW$wind.speed[which((datW$precipitation  >= 2 & datW$lightning.acvitivy >0)|(datW$precipitation > 5))]),
        "error: unequal length")
-assert(length(length((is.na(datW$wind.speedQ1)[which(is.na(datW$wind.speedQ1==TRUE))]))) == length(datW$wind.speedQ1),
-       "error: unequal length")
-assert(length(is.na(datW$wind.speedQ1)==FALSE) == length(datW$wind.speedQ1),
-       "error: unequal length")
-print(length((is.na(datW$wind.speedQ1)[which(is.na(datW$wind.speedQ1==TRUE))])))
-print(length(ifelse(datW$precipitation  < 2 & datW$lightning.acvitivy ==0, NA,
-                    ifelse(datW$precipitation < 5, NA, datW$wind.speed))))
+
 plot(datW$DD , datW$wind.speedQ1, xlab = "Day of Year", ylab = "Windspeed",
      type="n")
 points(datW$DD[datW$wind.speedQ1 > 0], datW$wind.speedQ1[datW$wind.speedQ1 > 0],
@@ -82,13 +76,55 @@ points(datW$DD[datW$wind.speedQ1 > 0], datW$wind.speedQ1[datW$wind.speedQ1 > 0],
 lines(datW$DD[datW$wind.speedQ1 > 0], datW$wind.speedQ1[datW$wind.speedQ1 > 0],
        col= "tomato3", pch=15) 
 #Question 7
+#QA/Qc ing soil mosture and soil temp data assuming that the values would be unreliable for instances of storms , just like air temp and moisture
 datW$soil.moistureQ1<-  ifelse(datW$precipitation  >= 2 & datW$lightning.acvitivy >0, NA,
                                ifelse(datW$precipitation > 5, NA, datW$soil.moisture))
 datW$soil.tempQ1<-  ifelse(datW$precipitation  >= 2 & datW$lightning.acvitivy >0, NA,
                                ifelse(datW$precipitation > 5, NA, datW$soil.temp))
-
+#plotting the QA/QC'ed data and comparing it with plot of non, QA/QC'ed data. If the same points are plotted , data leading uto shortage was reliable
 plot(datW$DD , datW$soil.moistureQ1, xlab = "Day of Year", ylab = "Soil Moisture",
      type="n")
-points(datW$DD[datW$soil.moistureQ1!= NA], datW$soil.moistureQ1[datW$soil.moistureQ1!= NA],
+lines(datW$DD[which(is.na(datW$soil.moistureQ1)== FALSE)], datW$soil.moistureQ1[which(is.na(datW$soil.moistureQ1)== FALSE)],
        col= rgb(95/255,158/255,160/255,.5), pch=15) 
+points(datW$DD[which(is.na(datW$soil.moistureQ1)== FALSE)], datW$soil.moistureQ1[which(is.na(datW$soil.moistureQ1)== FALSE)],
+      col= rgb(95/255,158/255,160/255,.5), pch=15)
 
+plot(datW$DD, datW$soil.moisture, pch=19, type="b", xlab = "Day of Year",
+     ylab="Soil moisture (cm3 water per cm3 soil)")
+plot(datW$DD , datW$soil.tempQ1, xlab = "Day of Year", ylab = "Soil Temperature",
+     type="n")
+lines(datW$DD[which(is.na(datW$soil.tempQ1)== FALSE)], datW$soil.tempQ1[which(is.na(datW$soil.tempQ1)== FALSE)],
+      col= rgb(95/255,158/255,160/255,.5), pch=15)
+points(datW$DD[which(is.na(datW$soil.tempQ1)== FALSE)], datW$soil.tempQ1[which(is.na(datW$soil.tempQ1)== FALSE)],
+      col= rgb(95/255,158/255,160/255,.5), pch=15)
+plot(datW$DD, datW$soil.temp, pch=19, type="b", xlab = "Day of Year",
+     ylab="Soil temperature")
+
+#question 8
+# average calculations with right amount of digits through rounding
+wind.speedAVG <- round(mean(datW$wind.speed, na.rm = TRUE), digits = 2)
+air.temperatureAVG <- round(mean(datW$air.temperature, na.rm = TRUE), digits = 1)
+soil.moistureAVG <- round(mean(datW$soil.moisture, na.rm = TRUE), digits = 3)
+soil.tempAVG  <- round(mean(datW$soil.temp, na.rm = TRUE), digits = 1)
+precipitationAVG <- round(mean(datW$precipitation, na.rm = TRUE), digits = 3)
+
+#creating the data frame with all the averages
+requestedAvgTable <- data.frame(wind.speedAVG, air.temperatureAVG, soil.moistureAVG, soil.tempAVG, precipitationAVG)
+print(requestedAvgTable)
+
+#have the 4 plots on one screen
+par(mfrow=c(2,2))
+
+#  X axis common to the 4 plots covers the days into the year 
+#ylab describes the variable being measured
+plot(datW$DD, datW$soil.moisture, pch=19, type="b", xlab = "Day of Year",
+     ylab="Soil moisture (cm3 water per cm3 soil)")
+
+plot(datW$DD, datW$air.temperature, pch=19, type="b", xlab = "Day of Year",
+     ylab="Air temperature (degrees C)")
+
+plot(datW$DD, datW$soil.temp, pch=19, type="b", xlab = "Day of Year",
+     ylab="Soil temperature (degrees C)")
+
+plot(datW$DD, datW$precipitation, pch=19, type="b", xlab = "Day of Year",
+     ylab="Precipitation (mm)")
